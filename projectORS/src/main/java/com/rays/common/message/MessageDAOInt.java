6f3f52d3d537ac5f81cd1b1b299945dbf35a9331
vineetgoyal29 <vineet.goyel@gmail.com>
2022-08-16 13:09:03 +0530

    msg

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Author Vineet Goyel
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
